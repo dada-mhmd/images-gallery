95d8c61204278a46b5f9b5bb72966903555caabc
dada-mhmd <moudi.ld@hotmail.com>
2022-05-30 15:26:29 +0300

    added images to the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './Components/Header'
 import Search from './Components/Search'
 
@@ -7,6 +7,7 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 const App = () => {
   const [term, setTerm] = useState('')
+  const [images, setImages] = useState([])
 
   const handleSearchSubmit = e => {
     e.preventDefault()
@@ -14,9 +15,10 @@ const App = () => {
       `https://api.unsplash.com/photos/random/?query=${term}&client_id=${UNSPLASH_KEY}`
     )
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setImages([data, ...images]))
       .catch(err => console.log(err))
 
+    console.log(images)
     setTerm('')
   }
 
