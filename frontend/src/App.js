3eda86f7747c2db2ceb4a3e614491aa716cc1541
dada-mhmd <moudi.ld@hotmail.com>
2022-05-31 11:15:47 +0300

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import Header from './Components/Header'
+import ImageCard from './Components/ImageCard'
 import Search from './Components/Search'
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
@@ -15,17 +17,29 @@ const App = () => {
       `https://api.unsplash.com/photos/random/?query=${term}&client_id=${UNSPLASH_KEY}`
     )
       .then(res => res.json())
-      .then(data => setImages([data, ...images]))
+      .then(data => setImages([{ ...data, title: term }, ...images]))
       .catch(err => console.log(err))
 
-    console.log(images)
     setTerm('')
+  }
+
+  const handleDeleteImage = id => {
+    setImages(images.filter(image => image.id !== id))
   }
 
   return (
     <>
       <Header title='Images Gallery' />
       <Search term={term} setTerm={setTerm} handleSubmit={handleSearchSubmit} />
+      <Container>
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className='pb-3'>
+              <ImageCard handleDelete={handleDeleteImage} image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   )
 }
--- frontend/src/Components/ImageCard.jsx
+++ frontend/src/Components/ImageCard.jsx
@@ -1,4 +1,21 @@
-const ImageCard = () => {
-  return <div>ImageCard</div>
+import { Button, Card } from 'react-bootstrap'
+
+const ImageCard = ({ image, handleDelete }) => {
+  return (
+    <Card className='m-5'>
+      <Card.Img
+        style={{ width: '15rem' }}
+        variant='top'
+        src={image.urls.small}
+      />
+      <Card.Body>
+        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant='primary' onClick={() => handleDelete(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  )
 }
 export default ImageCard
